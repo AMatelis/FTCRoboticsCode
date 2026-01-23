b63ceea1e3848101655987e329d094462dff189f
Pfish2028 <pfish7375@gmail.com>
2026-01-23 16:23:11 -0500

    Update Hardware.java

--- Hardware.java
+++ Hardware.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -14,19 +16,22 @@ public class Hardware
     public DcMotor backLeft;
     public DcMotor frontRight;
     public DcMotor backRight;
-    
-    // Declare Elevator
     public DcMotor elevator;
-    public DcMotor reverseElevator;
     
-    // Declare servos
-    public Servo grabber;
+    // Declare ballBlaster
+    public DcMotor ballBlaster;
+    public DcMotor intake;
+    
+    //camera
+    public WebcamName camera;
 
-    HardwareMap hardwareMap;
+    private HardwareMap hardwareMap;
     
     public void init(HardwareMap hardwareMap)    
     {
         // Initialize the hardware variables
+        this.hardwareMap = hardwareMap; 
+        
         
         //Motors 
         frontLeft = hardwareMap.get(DcMotor.class, "front_left");
@@ -34,13 +39,9 @@ public class Hardware
         frontRight = hardwareMap.get(DcMotor.class, "front_right");
         backRight = hardwareMap.get(DcMotor.class, "back_right");
         
-        // Elevator Motors
-        elevator = hardwareMap.get(DcMotor.class, "elevator");
-        reverseElevator = hardwareMap.get(DcMotor.class, "reverse_elevator");
-        intake = hardwareMap.get(DcMotor.class, "intake"); 
-        
-        // Servos
-        grabber = hardwareMap.get(Servo.class, "grabber");
+        // Activity motors
+        ballBlaster = hardwareMap.get(DcMotor.class, "ballShooter");
+        intake = hardwareMap.get(DcMotor.class, "intake");
 
         // Initialize direction of motors
         frontLeft.setDirection(DcMotor.Direction.REVERSE);
@@ -48,11 +49,12 @@ public class Hardware
         frontRight.setDirection(DcMotor.Direction.FORWARD);
         backRight.setDirection(DcMotor.Direction.FORWARD);
         
-        elevator.setDirection(DcMotor.Direction.FORWARD);
-        reverseElevator.setDirection(DcMotor.Direction.FORWARD); 
+        ballBlaster.setDirection(DcMotor.Direction.FORWARD);
+        intake.setDirection(DcMotor.Direction.REVERSE);
         
-        // Initialize Servos
-        grabber.setPosition(0);
+        //camera
+        camera = hardwareMap.get(WebcamName.class, "Webcam 1");
+        
+
     }
 }
-
